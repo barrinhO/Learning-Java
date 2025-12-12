669bdfae5f2dc00499b253efcdea76b324303588
= <=>
2025-12-12 14:49:04 -0300

    Pedir 5 números e retornar o maior

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,4 @@
 package org.example;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -9,37 +8,23 @@ public class Main {
     Scanner scan = new Scanner(System.in);
     ArrayList<Integer> numbers = new ArrayList<>();
 
-    
-    int actual_number = 1;
-
-    while(actual_number != 0){
-      try{
-
-        System.out.println("\nDigite um número: ");
-        int typedNumber = scan.nextInt();
-
-        actual_number = typedNumber;
-
-        numbers.add(actual_number);
-
-        System.out.println("\nNúmero digitado: " + actual_number);
-      }
-      catch (InputMismatchException err){
-        System.out.println("Digite números não string: " + err );
-        break;
-      }
+    System.out.println("\nDigite 5 números: ");
+    while(numbers.size() != 5){
+      int typedNumber = scan.nextInt();
+      numbers.add(typedNumber);
     }
 
-    System.out.println("\nNúmeros da lista: ");
+    int maiorNumero = numbers.get(0);
 
-
-    int soma = 0;
     for(int num : numbers){
-      soma += num;
-      System.out.println(num);
+
+      if(num > maiorNumero){
+        maiorNumero = num;
+      }
+
     }
 
-    System.out.println("Soma: " + soma);
+    System.out.println("Maior número: " + maiorNumero);
 
   }
 }
