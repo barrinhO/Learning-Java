f6c86ddc6a4c1491df558f36faf31bed79700a8c
= <=>
2025-12-22 23:42:37 -0300

    Invertendo string

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,21 @@
 package org.example;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    ArrayList<Float> notas = new ArrayList<>();
+    // inverter uma palavra
 
+    String word = "Java";
+    String reverseString = "";
 
-    System.out.println("Quantas notas são?\n");
-    int qtdNotas = scan.nextInt();
+    // stringbuilder tb pode ser usado
+    StringBuilder sb = new StringBuilder();
 
-    System.out.println("Qual a é a media?\n");
-    float mediaEscolar = scan.nextFloat();
+    for(int i = word.length() - 1; i >= 0; i--){
+      reverseString = reverseString + word.charAt(i);
+      sb.append(word.charAt(i));
 
-    System.out.println("Digite as " + qtdNotas + " notas\n");
-
-    for(int i = 1; i <= qtdNotas; i++){
-      float typedNota = scan.nextFloat();
-      System.out.println("Nota"+ i + ": " + typedNota);
-      notas.add(typedNota);
     }
-
-    float total = 0;
-
-    for(float notaLista : notas){
-      total += notaLista;
-    }
-
-    float mediaFinal = total / qtdNotas;
-
-    System.out.println("\nTotal de pontos: " + total);
-    System.out.println("Média: " + total/qtdNotas + "\n");
-
-    if(mediaFinal >= mediaEscolar){
-      System.out.println("Você foi aprovado!");
-    }
-    else{
-      System.out.println("Reprovado");
-    }
-
+    System.out.println(reverseString);
+    System.out.println(sb.toString());
   }
 }
